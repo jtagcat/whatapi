0697176d3d1c4df30d5119b814bb8ad7b0719bb7
Charles Haynes <ceh+gitlab@ceh.bz>
2018-11-20 21:07:52 -0500

    name TorrentStruct and TorrentSearchResultStruct

--- r_search.go
+++ r_search.go
@@ -35,54 +35,58 @@ type RequestsSearch struct {
 	} `json:"results"`
 }
 
+type TorrentStruct struct {
+	TorrentID int `json:"torrentId"`
+	EditionID int `json:"editionId"`
+	Artists   []struct {
+		ID      int    `json:"id"`
+		Name    string `json:"name"`
+		AliasID int    `json:"aliasid"`
+	} `json:"artists"`
+	Remastered              bool   `json:"remastered"`
+	RemasterYear            int    `json:"remasterYear"`
+	RemasterCatalogueNumber string `json:"remasterCatalogueNumber"`
+	RemasterTitle           string `json:"remasterTitle"`
+	Media                   string `json:"media"`
+	Encoding                string `json:"encoding"`
+	Format                  string `json:"format"`
+	HasLog                  bool   `json:"hasLog"`
+	LogScore                int    `json:"logScore"`
+	HasCue                  bool   `json:"hasCue"`
+	Scene                   bool   `json:"scene"`
+	VanityHouse             bool   `json:"vanityHouse"`
+	FileCount               int    `json:"fileCount"`
+	Time                    string `json:"time"`
+	Size                    int64  `json:"size"`
+	Snatches                int    `json:"snatches"`
+	Seeders                 int    `json:"seeders"`
+	Leechers                int    `json:"leechers"`
+	IsFreeleech             bool   `json:"isFreeleech"`
+	IsNeutralLeech          bool   `json:"isNeutralLeech"`
+	IsPersonalFreeleech     bool   `json:"isPersonalFreeleech"`
+	CanUseToken             bool   `json:"canUseToken"`
+}
+
+type TorrentSearchResultStruct struct {
+	GroupID       int             `json:"groupId"`
+	GroupName     string          `json:"groupName"`
+	Artist        string          `json:"artist"`
+	Tags          []string        `json:"tags"`
+	Bookmarked    bool            `json:"bookmarked"`
+	VanityHouse   bool            `json:"vanityHouse"`
+	GroupYear     int             `json:"groupYear"`
+	ReleaseType   string          `json:"releasetType"`
+	GroupTime     string          `json:"groupTime"`
+	TotalSnatched int             `json:"totalSnatched"`
+	TotalSeeders  int             `json:"totalSeeders"`
+	TotalLeechers int             `json:"totalLeechers"`
+	Torrents      []TorrentStruct `json:"torrents"`
+}
+
 type TorrentSearch struct {
-	CurrentPage int `json:"currentPage"`
-	Pages       int `json:"pages"`
-	Results     []struct {
-		GroupID       int      `json:"groupId"`
-		GroupName     string   `json:"groupName"`
-		Artist        string   `json:"artist"`
-		Tags          []string `json:"tags"`
-		Bookmarked    bool     `json:"bookmarked"`
-		VanityHouse   bool     `json:"vanityHouse"`
-		GroupYear     int      `json:"groupYear"`
-		ReleaseType   string   `json:"releasetType"`
-		GroupTime     string   `json:"groupTime"`
-		TotalSnatched int      `json:"totalSnatched"`
-		TotalSeeders  int      `json:"totalSeeders"`
-		TotalLeechers int      `json:"totalLeechers"`
-		Torrents      []struct {
-			TorrentID int `json:"torrentId"`
-			EditionID int `json:"editionId"`
-			Artists   []struct {
-				ID      int    `json:"id"`
-				Name    string `json:"name"`
-				AliasID int    `json:"aliasid"`
-			} `json:"artists"`
-			Remastered              bool   `json:"remastered"`
-			RemasterYear            int    `json:"remasterYear"`
-			RemasterCatalogueNumber string `json:"remasterCatalogueNumber"`
-			RemasterTitle           string `json:"remasterTitle"`
-			Media                   string `json:"media"`
-			Encoding                string `json:"encoding"`
-			Format                  string `json:"format"`
-			HasLog                  bool   `json:"hasLog"`
-			LogScore                int    `json:"logScore"`
-			HasCue                  bool   `json:"hasCue"`
-			Scene                   bool   `json:"scene"`
-			VanityHouse             bool   `json:"vanityHouse"`
-			FileCount               int    `json:"fileCount"`
-			Time                    string `json:"time"`
-			Size                    int64  `json:"size"`
-			Snatches                int    `json:"snatches"`
-			Seeders                 int    `json:"seeders"`
-			Leechers                int    `json:"leechers"`
-			IsFreeleech             bool   `json:"isFreeleech"`
-			IsNeutralLeech          bool   `json:"isNeutralLeech"`
-			IsPersonalFreeleech     bool   `json:"isPersonalFreeleech"`
-			CanUseToken             bool   `json:"canUseToken"`
-		} `json:"torrents"`
-	} `json:"results"`
+	CurrentPage int                         `json:"currentPage"`
+	Pages       int                         `json:"pages"`
+	Results     []TorrentSearchResultStruct `json:"results"`
 }
 
 type UserSearch struct {
